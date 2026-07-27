1f1c011042305d7b7560bd4a6da798e0ba865d99
Hassan1Abdallah <hsna63667@gmail.com>
2026-07-28 01:11:39 +0300

    Add new files in Application layer

---
+++ src/MechanicShop.Application/Common/Models/PageinatedList.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MechanicShop.Application.Common.Models
+{
+    public class PageinatedList<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+        public IReadOnlyCollection<T>? Items { get; set; }
+
+    }
+}
