bc711c8113004c063fa7682a09be3c356cb04f6c
Hassan1Abdallah <hsna63667@gmail.com>
2026-07-28 17:45:00 +0300

    start CQRS

---
+++ src/MechanicShop.Application/Features/Customers/Dtos/VehicleDto.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace MechanicShop.Application.Features.Customers.Dtos
+{
+    public sealed record VehicleDto(Guid VehicleId,
+                                    string Makem,
+                                    string Model,
+                                    int Year,
+                                    string LicensePlate);
+}
---
+++ src/MechanicShop.Application/Features/Customers/Mappers/CustomerMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanicShop.Application.Features.Customers.Mappers
+{
+    internal class CustomerMapper
+    {
+    }
+}
